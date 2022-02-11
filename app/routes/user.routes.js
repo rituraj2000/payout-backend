48e6f522f24722c9a8af2d2cc840ad4eed8e9021
Arhaan Bahadur <arhaanb@gmail.com>
2022-02-11 21:08:54 +0530

    chat schema

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -6,6 +6,7 @@ const app = express.Router()
 
 const User = require('../models/user.model')
 const Company = require('../models/company.model')
+const Thread = require('../models/threads.model')
 
 // app.use(function (req, res, next) {
 // 	res.header(
@@ -82,24 +83,177 @@ app.get('/comp/:compname', [authJwt.verifyToken], (req, res) => {
 			return res.status(500).send({ message: 'ERROR' })
 		}
 		var isOwned = false
-		if (company.creatorID == req.userId) {
+		if (company?.creatorID == req.userId) {
 			isOwned = true
 		}
-		return res.send({ company, isOwned })
+
+		Thread.findOne({
+			$and: [
+				{ $or: [{ p1: req.userId }, { p2: req.userId }] },
+				{ $or: [{ p1: company.creatorID }, { p2: company.creatorID }] }
+			]
+		}).exec((err, thread) => {
+			if (err) {
+				console.log(err)
+				return res.status(500).send({ message: 'ERROR' })
+			}
+
+			User.findOne({ _id: company.creatorID }, { password: 0, roles: 0 }).exec(
+				(err, userdata) => {
+					if (err) {
+						console.log(err)
+						return res.status(500).send({ message: 'ERROR' })
+					}
+
+					if (thread) {
+						return res.send({
+							company,
+							isOwned,
+							owner: userdata,
+							threadStarted: true,
+							thread
+						})
+					} else {
+						return res.send({
+							company,
+							isOwned,
+							owner: userdata,
+							threadStarted: false
+						})
+					}
+				}
+			)
+		})
 	})
-	// User.findById(req.userId, { password: 0 }).exec((err, user) => {
-	// 	if (err) {
-	// 		console.log(err)
-	// 		return res.status(500).send({ message: 'ERROR' })
-	// 	}
-	// 	Company.find({ creatorID: req.userId }).exec((err, companies) => {
-	// 		if (err) {
-	// 			console.log(err)
-	// 			return res.status(500).send({ message: 'ERROR' })
-	// 		}
-	// 		return res.send({ user, companies })
-	// 	})
-	// })
+})
+
+//get user information
+app.get('/user/:userid', [authJwt.verifyToken], (req, res) => {
+	const userid = req.params.userid
+	User.findOne({ _id: userid }, { password: 0 }).exec((err, userdata) => {
+		if (err) {
+			console.log(err)
+			return res.status(500).send({ message: 'ERROR' })
+		}
+
+		return res.send({ userdata })
+	})
+})
+
+//get all users
+app.get('/users', [authJwt.verifyToken], (req, res) => {
+	User.find(
+		{},
+		{ password: 0, roles: 0, salt: 0, hash: 0, createdAt: 0, updatedAt: 0 }
+	).exec((err, userdata) => {
+		if (err) {
+			console.log(err)
+			return res.status(500).send({ message: 'ERROR' })
+		}
+
+		return res.send({ users: userdata })
+	})
+})
+
+//start a texting thread
+app.post('/create/thread', [authJwt.verifyToken], (req, res) => {
+	var threadData = {
+		p1: req.userId,
+		p2: req.body.p2,
+		p1seen: true,
+		p2seen: false,
+		messages: [
+			{
+				content: req.body.content,
+				from: req.userId,
+				date: req.body.date
+			}
+		],
+		lastMessage: req.body.date
+	}
+
+	Thread.create(threadData, (error, log) => {
+		if (error) {
+			console.log(error)
+			return res.status(400).send({ error })
+		}
+		console.log('company created')
+		return res.send('text thread created')
+	})
+})
+
+//send a message (in existing thread)
+app.post('/sendmsg', [authJwt.verifyToken], (req, res) => {
+	msgdata = {
+		content: req.body.content,
+		from: req.userId,
+		date: req.body.date
+	}
+
+	Thread.findOne({ _id: req.body.threadid }).exec((err, threaddata) => {
+		if (err) {
+			console.log(err)
+			return res.status(500).send({ message: 'ERROR' })
+		}
+
+		threaddata.messages.push(msgdata)
+		threaddata.lastMessage = req.body.date
+		if (req.userId == p1) {
+			threaddata.p1seen = true
+			threaddata.p2seen = false
+		} else {
+			threaddata.p2seen = true
+			threaddata.p1seen = false
+		}
+		threaddata.save()
+
+		return res.send({ thread: threaddata })
+	})
+})
+
+//set seen indication
+app.post('/set/seen', [authJwt.verifyToken], (req, res) => {
+	Thread.findOne({ _id: req.body.threadid }).exec((err, threaddata) => {
+		if (err) {
+			console.log(err)
+			return res.status(500).send({ message: 'ERROR' })
+		}
+
+		if (req.userId == p1) {
+			threaddata.p1seen = true
+		} else {
+			threaddata.p2seen = true
+		}
+		threaddata.save()
+
+		return res.send({ msg: 'set seen indication', thread: threaddata })
+	})
+})
+
+//get all threads
+app.get('/threads', [authJwt.verifyToken], (req, res) => {
+	Thread.find({ $or: [{ p1: req.userId }, { p2: req.userId }] }).exec(
+		(err, threads) => {
+			if (err) {
+				console.log(err)
+				return res.status(500).send({ message: 'ERROR' })
+			}
+
+			return res.send({ threads })
+		}
+	)
+})
+
+//get specific thread
+app.get('/threads/:threadid', [authJwt.verifyToken], (req, res) => {
+	Thread.findOne({ _id: req.params.threadid }).exec((err, threads) => {
+		if (err) {
+			console.log(err)
+			return res.status(500).send({ message: 'ERROR' })
+		}
+
+		return res.send({ threads })
+	})
 })
 
 // app.get(
